6f91eaec033408fd81b113bfe0cc44e1687949ca
Alerdo <alerdo23@gmail.com>
2022-11-14 14:13:30 +0000

    First Commit for the Mixed Messages

---
+++ joke.js
@@ -0,0 +1,52 @@
+
+//We create a random nr generator 
+function random(num) {
+    return  Math.floor(Math.random() * num); 
+}
+
+
+
+///Create outcomes with diffrent outcomes
+
+const outcomes = {
+    day: ['Monday', 'Tuesday', 'Wedensday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    weather: ['Sunny', 'Cloudy', 'Hot', 'Windy'],
+    joke: ["Ha ha ha ", 'Bla bla bla', 'He he he']
+}
+
+/// Create output array
+const personalWisdom = [];
+
+
+
+for( prop in outcomes) {
+    let propIndex = random(outcomes[prop].length) //finds random index of properties
+
+    switch(prop){
+        case 'day':
+            personalWisdom.push(`Today is ${outcomes[prop][propIndex]}`)
+            break;
+        
+        case 'weather':
+            personalWisdom.push(`I am happy becase I like ${outcomes[prop][propIndex]} weather`)
+            break;
+        
+        case 'joke':
+            personalWisdom.push(`Hear me out: ` + outcomes[prop][propIndex])
+            break;
+        
+        default:
+            personalWisdom.push(`Not enough info to start to implement program`)
+            break;
+    }
+}
+
+
+
+const formatText = text => {
+    let edited = text.join('\n')
+    console.log(edited)
+}
+
+
+formatText(personalWisdom)
